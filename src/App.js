73058f012c6654a7a280618b29f6c2446bad1c89
Myles O'Connor <hello@myles.la>
2017-10-07 14:34:40 -0700

    Set up React app to display websocket message in real time

--- src/App.js
+++ src/App.js
@@ -3,42 +3,47 @@ import Websocket from 'react-websocket';
 import logo from './logo.svg';
 import './App.css';
 
+const WEBSOCKET_URL = 'ws://localhost:8080';
+
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            count: 90
+            message: null
         };
     }
 
     onOpen = () => {
-        console.log('onOpen() fired.');
+        console.log('Connection opened.');
     }
 
     handleData = (data) => {
-        let result = JSON.parse(data);
-
-        this.setState({
-            count: this.state.count + result.movement
-        });
+        this.setState({ message: data });
     }
 
     onClose = () => {
-        console.log('onClose() fired.');
+        // Connection closed, set message back to null
+        this.setState({ message: null });
     }
 
     render() {
+        const { message } = this.state;
+
         return (
             <div className="App">
                 <header className="App-header">
                     <img src={ logo } className="App-logo" alt="logo" />
-                    <h1 className="App-title">Testing Websocket connection</h1>
+                    <h1 className="App-title">Testing websocket connection</h1>
                 </header>
                 <p className="App-intro">
-                    Count: <strong>{ this.state.count }</strong>
+                    {
+                        (message !== null)
+                        ? <p><strong>{ message }</strong></p>
+                        : <p>No data coming from websocket</p>
+                    }
                 </p>
                 <Websocket
-                    url="ws://localhost:8888/live/product/12345/"
+                    url={ WEBSOCKET_URL }
                     onOpen={ this.onOpen }
                     onClose={ this.onClose }
                     onMessage={ this.handleData }
